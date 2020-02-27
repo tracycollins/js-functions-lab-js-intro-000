159e5cc6c40f53a497848c8c003684af0c343641
Tracy Collins <tc@threeceemedia.com>
2020-02-27 03:50:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function (holiday, name){
+function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
 }
